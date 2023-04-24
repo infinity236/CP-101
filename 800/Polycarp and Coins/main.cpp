e0d46341cecca9fce0bd6cf134072e8d54d30d07
Walid <walid@Walid.local>
2023-04-24 15:17:50 +0600

    Polycarp and Coins |Solve

--- 800/Polycarp and Coins/main.cpp
+++ 800/Polycarp and Coins/main.cpp
@@ -1,8 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
+void solve()
+{
+    int n; cin>>n;
+    int a, b;
+    int res = n/3;
+    a = res; b= res;
 
-int main(){
+    if(n%3 == 1){
+        a++;
+    }
+    else if(n%3 == 2){
+        b++;
+    }
+    cout << a << " " << b << endl;
+}
 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
     return 0;
 }
