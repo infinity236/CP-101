82a676d25dd822fc46c9b7907a4f597502b8071b
Walid <walid@Walid.local>
2023-04-24 15:45:12 +0600

    Odd Set | Solve

--- 800/Odd Set/main.cpp
+++ 800/Odd Set/main.cpp
@@ -1,8 +1,25 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
+void solve()
+{
+    int n; cin>>n;
+    int ans=0;
+    for(int i=0; i<(2*n); i++){
+        int x; cin>>x;
+        if (x%2 == 1) ans++;
+    }
+
+    if(ans == n) cout << "YES" << endl;
+    else cout << "NO" << endl; 
+
+}
 int main(){
 
+    int t; cin>> t;
+    while(t--)
+    {
+        solve();
+    }
     return 0;
 }
