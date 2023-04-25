2b3157981d9645cd2cc9fd9ea2f2a65726650eee
Walid <walid@Walid.local>
2023-04-25 12:01:07 +0600

    Arithmetic Array | Day 2

---
+++ 800/Arithmetic Array/main.cpp
@@ -0,0 +1,47 @@
+/*
+    Problem: Arithmetic Array
+    Link: https://codeforces.com/problemset/problem/1537/A
+
+    Solution:
+    1. Find the sum of all elements in the array
+    2. If the sum is equal to n, then print 0
+    3. If the sum is less than n, then print 1
+    4. If the sum is greater than n, then print the difference between the sum and n
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+double sumArray(int *arr, int n){
+    int sum = 0;
+    for(int i=0; i<n; i++)
+    {
+        sum += *(arr+i);
+    }
+    return sum;
+}
+
+void solve(){
+
+    int n; cin >>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    double arithmeticMean = (sumArray(arr, n))/n;
+    
+    if(arithmeticMean == 1) cout << "0" << endl;
+    else if(arithmeticMean < 1) cout << "1" << endl;
+    else cout << (sumArray(arr, n) - n) << endl; 
+    
+}
+
+int main(){
+    int t; cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Level: 800
 1. [Polycarp and Coins] https://codeforces.com/problemset/problem/1551/A
 2. [Odd Set] https://codeforces.com/problemset/problem/1542/A
+3. [Arithmetic Array] https://codeforces.com/problemset/problem/1537/A
